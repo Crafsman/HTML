48520de71b20d7effdffb9f3d50035a36e748b8a
Crafsman <ymq655@gmail.com>
2018-04-09 14:06:42 +1000

    delete test file

--- os_path.py
+++
@@ -1,8 +0,0 @@
-#http://blog.csdn.net/middlekingt/article/details/8593053
-import os
-
-file_name = 'test_file'
-isexist = os.path.exists(file_name)#judge the spicific folder is exist
-if isexist == False:               #if not exist
-    print(isexist)
-    os.makedirs(file_name)         #then make the corresponding folder
